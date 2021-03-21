6f797b71850d27bf12631ec259f6b9d3fed44714
GabrielPerez-Dev <wiredgear@gmail.com>
2021-03-20 20:18:55 -0400

    Added player input functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 3.5f;
+
     private void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(xInput, yInput, 0);
+
+        transform.Translate(movement.normalized * _speed * Time.deltaTime);
     }
 }
