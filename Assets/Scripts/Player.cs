4e6d509dcc5539b656b8d3459d7d67b387fa65c4
GabrielPerez-Dev <wiredgear@gmail.com>
2021-03-22 05:52:59 -0400

    Refactored the shoot logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.5f;
-    [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private float _fireRate = 0.15f;
     [SerializeField] private GameObject _projectilePrefab = null;
+    private float _canFire = -1f;
     private const float BoundY = 6.5f;
     private const float WrapX = 13f;
 
@@ -19,11 +20,18 @@ public class Player : MonoBehaviour
         VerticalScreenBounds();
         HorizontalScreenWrap();
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            Vector3 offsetY = new Vector3(0, 1f, 0);
-            Instantiate(_projectilePrefab, transform.position + offsetY, Quaternion.identity);
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        _canFire = Time.time + _fireRate;
+
+        Vector3 offsetY = new Vector3(0, 1f, 0);
+        Instantiate(_projectilePrefab, transform.position + offsetY, Quaternion.identity);
     }
 
     private void HorizontalScreenWrap()
